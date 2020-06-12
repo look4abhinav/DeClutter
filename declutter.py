c6e8f8f1aca404e5e3f473405610a07c7d70d76c
look4abhinav <talk2abhinav@yahoo.com>
2020-06-13 02:55:24 +0530

    Added print messages

--- declutter.py
+++ declutter.py
@@ -15,19 +15,27 @@ src_path = os.getcwd()
 dest_path = os.path.join(src_path,'DeClutter')
 try:
 	os.mkdir('DeClutter')
+	print('Creating DeClutter directory')
 	for ext in formats.keys():
+		print('Creating',ext.capitalize(),'directory')
 		os.mkdir('./DeClutter/'+ext.capitalize())
+	print('Getting file paths')
 	path = [os.path.abspath(_) for _ in os.listdir(src_path) if not os.path.isdir(_)]
 	for file in path:
 		if file != __file__:
 			fileType = getFileType(file)
 			for types in formats.keys():
 				if fileType in formats[types]:
+					print('Moving {} to {} directory'.format(os.path.basename(file),types.capitalize()))
 					shutil.move(file,os.path.join(dest_path,types.capitalize()))
 
 except FileExistsError:
+	print('Moving all files to',src_path)
 	paths = [folders[0] for folders in os.walk(dest_path)]
 	for path in paths[::-1]:
 			for file in os.listdir(path):
+				print('Moving',os.path.basename(file))
 				shutil.move(os.path.join(path,file), src_path)
 			os.rmdir(path)
+	print('Removing DeClutter')
+	
